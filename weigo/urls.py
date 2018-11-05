7fcf9d688db7c6fd225b9273b28ca4bf85e2e38a
SeulYoung <948170997@qq.com>
2018-11-05 15:41:00 +0800

    Function complete except UI

--- weigo/urls.py
+++ weigo/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('registration.html/', views.registration, name='registration'),
     path('logout/', views.logout, name='logout'),
     path('profile.html/', views.profile, name='profile'),
+    path('emailUpdate.html/', views.email_update, name='email_update'),
+    path('passwordUpdate.html/', views.password_update, name='password_update'),
 ]
--- weigo/views.py
+++ weigo/views.py
@@ -1,7 +1,6 @@
 import re
 
 from django.contrib import auth
-from django.db import IntegrityError
 from django.shortcuts import render, redirect
 
 from weigo.models import MyUser
@@ -71,6 +70,41 @@ def profile(request):
         info = MyUser.objects.filter(email=request.user.email).first()
         return render(request, 'profile.html', {'email': info.email, 'username': info.username})
     return redirect('/login.html')
+
+
+def email_update(request):
+    if request.method == "POST":
+        email = request.POST.get('email')
+        email_valid = r'^[0-9a-zA-Z\_\-]+(\.[0-9a-zA-Z\_\-]+)*@[0-9a-zA-Z]+(\.[0-9a-zA-Z]+){1,}$'
+
+        if not re.match(email_valid, email):
+            return render(request, 'emailUpdate.html', {'email_error': 'enter a valid email address.'})
+        info = MyUser.objects.filter(email=email).first()
+        if info is not None:
+            return render(request, 'emailUpdate.html', {'email_error': 'email already taken.'})
+        MyUser.objects.filter(email=request.user.email).update(email=email)
+        return redirect('/login.html')
+
+    return render(request, 'emailUpdate.html')
+
+
+def password_update(request):
+    if request.method == "POST":
+        old_password = request.POST.get('old_password')
+        password = request.POST.get('password')
+        password_confirmation = request.POST.get('password_confirmation')
+
+        if not request.user.check_password(old_password):
+            return render(request, 'passwordUpdate.html', {'password_error': 'invalid password.'})
+        if len(password) < 6:
+            return render(request, 'passwordUpdate.html', {'password_error': 'password too short.'})
+        if password != password_confirmation:
+            return render(request, 'passwordUpdate.html', {'password_error': 'password mismatch.'})
+        request.user.set_password(password)
+        request.user.save()
+        return redirect('/login.html')
+
+    return render(request, 'passwordUpdate.html')
 
 
 def logout(request):
